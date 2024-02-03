4d3fdf8354fd0f6be1a8c0d00ac7e7bcd255524b
Bartosz Kaczyński <bkaczynski@posteo.net>
2024-02-03 22:44:28 +0100

    Add some Gnus defaults

--- custom.el
+++ custom.el
@@ -9,9 +9,7 @@
  '(completions-detailed t)
  '(create-lockfiles nil)
  '(custom-enabled-themes '(modus-vivendi-tritanopia))
- '(default-frame-alist
-   '((fullscreen . maximized) (font . "Cascadia Code 10")
-     (vertical-scroll-bars)))
+ '(default-frame-alist '((fullscreen . maximized) (font . "Cascadia Code 10")))
  '(delete-selection-mode t)
  '(dired-listing-switches "-alth")
  '(eshell-modules-list
@@ -21,6 +19,12 @@
 		  eshell-script eshell-term eshell-tramp eshell-unix))
  '(fido-vertical-mode t)
  '(global-hl-line-mode t)
+ '(gnus-always-read-dribble-file t)
+ '(gnus-gcc-mark-as-read t)
+ '(gnus-inhibit-startup-message t)
+ '(gnus-large-newsgroup nil)
+ '(gnus-select-method '(nnnil ""))
+ '(gnus-use-full-window nil)
  '(inhibit-startup-screen t)
  '(load-prefer-newer t)
  '(make-backup-files nil)
