d4eaf44ae3b40becb8322c37cf27e61060e711cf
Bartosz Kaczyński <bkaczynski@posteo.net>
2024-02-04 15:01:16 +0100

    Enable password-store backend

--- custom.el
+++ custom.el
@@ -7,6 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-sources '(password-store))
  '(auto-save-default nil)
  '(blink-cursor-mode nil)
  '(calendar-date-style 'european)
